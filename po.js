9e46566e7bf79cc0b34f1496cd8a524b8f339917
Shelby <snoonan@tbearsiberians.com>
2020-01-19 00:25:06 -0800

    po.css
    Basic hot-seat working

--- po.js
+++ po.js
@@ -37,11 +37,13 @@ var game = {
 };
 
 function bid_pass() {
+    console.log("bid_pass");
    game.bidders -= 1;
    game.players[game.player_list[game.auction_idx]].pass = true;
    next_auction();
 }
 function bid() {
+    console.log("bid");
    var v = +$('#bid').val();
    if (v <= game.auction_value) {
       return;
@@ -57,15 +59,28 @@ function bid() {
 }
 
 function next_auction() {
+    console.log("next_auction");
    if (game.bidders == 0) {
       // Unwanted, return to pool.
       next_action();
    }
-   game.auction_idx += 1;
-   if (game.auction_idx == game.max_player) {game.auction_idx = 0;}
-   if ( game.players[game.player_list[game.auction_idx]].pass) {
-      next_auction();
-      return;
+   var start_idx = game.auction_idx;
+   do {
+       game.auction_idx += 1;
+       if (game.auction_idx == game.max_player) {game.auction_idx = 0;}
+       if (! game.players[game.player_list[game.auction_idx]].pass) {
+          break;
+      }
+   } while (start_idx != game.auction_idx);
+   if (start_idx == game.auction_idx) {
+       if (game.players[game.player_list[game.auction_idx]].pass) {
+           game.bidders = 1;
+       } else {
+           game.bidders = 0;
+           // Unwanted, return to pool.
+           next_action();
+           return;
+       }
    }
    game.auction_p = game.player_list[game.auction_idx];
    if (game.bidders == 1 && game.auction_value > 0) {
@@ -74,12 +89,13 @@ function next_auction() {
       game.bid_share = undefined;
       $('#auction').hide();
       $('#turn').show();
-      if (game.starting < game_cfg.companies.length) {
+      if (game.depth == 3) {
+         // In initial auction, winner offers next.
          game.player_id = game.auction_winner;
-         _offer_stock(game_cfg.companies[game.starting][1]);
-         game.starting += 1;
+         _offer_stock(game_cfg.companies[game.sold_in_depth][1]);
          return;
-      } else if (game.starting == game_cfg.companies.length) {
+      } else if (game.depth == 2 && game.sold_in_depth == 0) {
+         // Initial auction done, start game proper.
          next_turn();
          return;
       }
@@ -89,19 +105,27 @@ function next_auction() {
    $('#bid').val(game.auction_value + 1);
 }
 function offer_stock(c) {
+    console.log("offer_stock");
+   var name = this.id.split('_')[0]
    if (game.bid_share || game.place_co) {
       // Already doing some action
       return;
    }
-   var name = this.id.split('_')[0]
+   if (game.depth != game.companies[name].stock) {
+      // Company already has less shares then peers.
+      return;
+   }
    _offer_stock(name);
 }
 function _offer_stock(name) {
+    console.log("_offer_stock");
    game.bid_share = name;
    game.auction_value = -1;
    game.auction_winner = undefined;
+   $('#h_bidder').text("None");
+   $('#h_bid').text(0);
    $('#bid_co').text(game.companies[name].fullname);
-   $('#bid_mark').attr('style','background:'+game.companies[name].color);
+   $('#bid_mark').addClass(name);
    $('#bid').val(0);
    $('#auction').show();
    $('#turn').hide();
@@ -114,6 +138,7 @@ function _offer_stock(name) {
    next_auction();
 }
 function buy_stock(c,p,m) {
+    console.log("buy_stock",c,p,m);
    game.companies[c].stock -=1;
    $('#'+c+'_stock').text(game.companies[c].stock);
    game.companies[c].holders.push(p);
@@ -122,10 +147,16 @@ function buy_stock(c,p,m) {
    game.players[p].shares.push(c);
    game.players[p].income += game.companies[c].income;
    $('#p'+p+'_income').text(game.players[p].income);
-   $('#p'+p+'_shares').append($('<span/>', {"style":"background:"+game.companies[c].color, html:c}));
+   $('#p'+p+'_shares').append($('<span/>', {"class":c, html:c}));
+   game.sold_in_depth += 1;
+   if (game.sold_in_depth == game_cfg.companies.length) {
+       game.depth -= 1;
+       game.sold_in_depth = 0;
+   }
 }
 
 function next_turn() {
+    console.log("next_tyrn");
    var list = [];
    game.players.forEach(function(p) {
       if (p.id == undefined) {return;}
@@ -143,42 +174,56 @@ function next_turn() {
       count += 1;
    });
    game.actions = [];
+   $('#actions').children().remove();
    for(var c = 0; c < game.max_player; c++) {
        var p = cup.splice(Math.random() * cup.length, 1)[0];
        game.actions.push(p);
-       $('#actions').append($('<span/>', {"p":p, text:game.players[p].name}));
+       $('#actions').append($('<span/>', {"p":p, text:" "+game.players[p].name}));
    }
    next_action();
 }
 function end_action() {
+    console.log("end_action");
    if (game.merge) {
       // A merge event happened, pay out.
       // Pay everyone.
       $.each(game.companies, function(c) {
-         game.companies[c].holders.forEach(function (h) {
-            // SPECIAL: BHEG needs to be at[8,2] and game.berlin to pay
-            if (c == 'BHEG' && (game.companies[c].city.indexOf('8_2') == -1 ||
-                                game.companies[c].city.indexOf(game.berlin[0]+'_'+game.berlin[1]) == -1)) {
-               return;
+          console.log(c);
+        // SPECIAL: BHEG needs to be at[7,3] and game.berlin to pay
+        if (     c == 'BHEG'
+             && (   !(game.loc.hasOwnProperty('7_3') && game.loc['7_3'].indexOf('BHEG') != -1)
+                 || !(game.loc.hasOwnProperty(game.berlin[0]+'_'+game.berlin[1])
+                    && game.loc[game.berlin[0]+'_'+game.berlin[1]].indexOf('BHEG') != -1))) {
+                return;
             }
+         console.log("pay");
+         game.companies[c].holders.forEach(function (h) {
             update_p_money(h, +game.companies[c].income);
          });
       });
-      // Pay touching co again.
-      game.companies[game.placing_co].holders.forEach(function (h) {
-         // SPECIAL: BHEG needs to be at[8,2] and game.berlin to pay
-         if (game.placing_co == 'BHEG' &&
-              (game.companies[game.placing_co].city.indexOf('8_2') == -1 ||
-               game.companies[game.placing_co].city.indexOf(game.berlin[0]+'_'+game.berlin[1]) == -1)) {
-            return;
-         }
-         update_p_money(h, +game.companies[game.placing_co].income);
-      });
+      if (game.placing_co) {
+          // Pay touching co again.
+          // SPECIAL: BHEG needs to be at[7,3] and game.berlin to pay
+              console.log("double pay",game.placing_co);
+          if (     game.placing_co == 'BHEG'
+               && (   !(game.loc.hasOwnProperty('7_3') && game.loc['7_3'].indexOf('BHEG') != -1)
+                   || !(game.loc.hasOwnProperty(game.berlin[0]+'_'+game.berlin[1])
+                      && game.loc[game.berlin[0]+'_'+game.berlin[1]].indexOf('BHEG') != -1))) {
+              // Skip second payment
+          } else {
+             console.log("pay");
+              game.companies[game.placing_co].holders.forEach(function (h) {
+                 update_p_money(h, +game.companies[game.placing_co].income);
+              });
+          }
+      }
    }
    next_action();
 }
 function next_action() {
+    console.log("next_action");
    game.placing_co = undefined;
+   game.turn_cost_total = 0;
    if (game.actions.length == 0) {
       next_turn();
       return;
@@ -190,18 +235,22 @@ function next_action() {
       $('#'+c+'_cubes').addClass('valid');
    });
 
+   $('#placing').hide();
    $('#action').children().remove();
    $('#action').append($('#actions').children()[0]);
 }
 function update_p_money(p, v) {
+    console.log("update_p_money",p,v);
    game.players[p].money += +v;
    $('#p'+p+'_money').text(game.players[p].money);
 }
 function update_c_money(c, v) {
+    console.log("update_c_money",c,v);
    game.companies[c].money += +v;
    $('#'+c+'_money').text(game.companies[c].money);
 }
 function add_income(c, v) {
+    console.log("add_income",c,v);
    // SPECIAL: MWB doubles biggest city.
    if (c == "MWB") {
       if (v > (game.companies[c].biggest_city||0)) {
@@ -220,18 +269,21 @@ function add_income(c, v) {
    });
 }
 function place_cube(c,x,y) {
+    console.log("place_cube",c,x,y);
   if (!game.loc[x+'_'+y]) { game.loc[x+'_'+y] = []; }
   game.loc[x+'_'+y].push(c);
-  $('#'+x+'_'+y+" .middle").append($('<span/>', {"style":"background:"+game.companies[c].color, html:"&nbsp;"}));
+  $('#'+x+'_'+y+" .middle").append($('<span/>', {"class":c, html:"&nbsp;"}));
   game.companies[c].cubes -= 1;
   $('#'+c+'_cubes').text(game.companies[c].cubes);
 }
-
+0
 function touch(a,b) {
-  $('#'+a+'_touch').append($('<span/>', {"style":"background:"+game.companies[b].color, html:"&nbsp;"}));
+    console.log("touch",a,b);
+  $('#'+a+'_touch').append($('<span/>', {"class":b, html:"&nbsp;"}));
   game.companies[a].touched[b] = true;
 }
 function place_cubes(c) {
+    console.log("place_cubes",c);
    if (game.bid_share || game.place_co) {
       // Already doing some action
       return;
@@ -254,9 +306,13 @@ function place_cubes(c) {
    if (name == 'GBS') { game.placing_free = true; }
    // Limit to cubes on hand.
    game.placing_cubes = Math.min(game.companies[name].cubes, game.placing_cubes);
-   alert('placing '+game.placing_cubes+" for "+fullname);
+   $('#place_count').text(game.placing_cubes);
+   $('#place_mark').addClass(name);
+   $('#place_mark').text(name);
+   $('#placing').show();
 }
 function clickhex(h) {
+    console.log("clickhex",h);
    var loc;
    var e;
 
@@ -319,16 +375,13 @@ function clickhex(h) {
     if (game.placing_co == 'KBS') {
        cost -= 1;
     }
-    if (game.companies[game.placing_co].cash >= cost &&
+    if (game.companies[game.placing_co].money < cost
        // SPECIAL CMEG can only spend 5.
-        (game.place_cost+cost < 5 || game.placing_co != 'CMEG'))
+       || (game.turn_cost_total+cost > 5 && game.placing_co == 'CMEG'))
     {
        return;
     }
 
-    if (cost > game.companies[game.placing_co].money) {
-       return;
-    }
     if (city == 'b') {
        if (game.companies[game.placing_co].bez == true) {
           return;
@@ -351,6 +404,7 @@ function clickhex(h) {
        game.city[x+'_'+y].push(game.placing_co);
        add_income(game.placing_co, +city);
     }
+    game.turn_cost_total += cost;
     place_cube(game.placing_co, x, y);
     update_c_money(game.placing_co, -cost);
     game.placing_cubes -= 1;
@@ -362,6 +416,7 @@ function clickhex(h) {
 
 function start()
 {
+    console.log("start");
    var map = $('#map');
    var table;
    var x,y;
@@ -460,7 +515,7 @@ function start()
       game.auction_list.push(name);
 
       row = $("<tr>");
-      cell = $("<td/>", {text:name, style:'background:'+color+';', 'title':special});
+      cell = $("<td/>", {text:name, 'class':name, 'title':special});
       row.append(cell);
       cell = $("<td/>", {id:name+'_stock', 'class':'auction', text:company.stock});
       row.append(cell);
@@ -474,7 +529,7 @@ function start()
       row.append(cell);
 
       table.append(row);
-      // Place starting city.
+      // Place sold_in_depth city.
       place_cube(name, loc[0], loc[1]);
       game.city[loc[0]+'_'+loc[1]] = [name];
       add_income(name, +$('#'+loc[0]+'_'+loc[1]+' .middle').attr('city'));
@@ -482,14 +537,15 @@ function start()
    $('.auction').click(offer_stock);
    $('.place').click(place_cubes);
 
-   game.starting = 0;
+   game.depth = game.companies['PO'].stock;
+   game.sold_in_depth = 0;
    game.player_id = 0;
-   _offer_stock(game_cfg.companies[game.starting][1]);
-   game.starting += 1;
+   _offer_stock(game_cfg.companies[game.sold_in_depth][1]);
 }
 
 function update_name(p)
 {
+    console.log("update_name",p);
    var ap = document.getElementById("p"+p+"_name");
 }
 
